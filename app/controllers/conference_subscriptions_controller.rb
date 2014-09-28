d708bc0139f0e19fd453ec9bd24e489b2c616662
Benny Degezelle <hi@monkeypatch.be>
2014-09-28 15:30:05 +0200

    Handle adding to and removing from conference groups for members

--- app/controllers/conference_subscriptions_controller.rb
+++ app/controllers/conference_subscriptions_controller.rb
@@ -19,4 +19,35 @@ class ConferenceSubscriptionsController < ReaderActionController
     end
     
   end
+  
+  def update
+    @conference_subscription = current_reader.conference_subscription
+    # if @conference_subscription.paid?
+    # else
+      @conference_subscription.update_attributes(params[:conference_subscription])
+      # Reset conference groups
+      current_reader.memberships.select{|m| m.group.ancestors.include?(conference_group) || m.group == conference_group}.each{|m| m.destroy}
+    
+      params["group_ids"].each do |id|
+        group = Group.find(id)
+        unless !group.ancestors.include?(conference_group)
+          current_reader.groups << group 
+          # look for day options
+          if id = params["conference_day_#{group.id}_option"]
+            group = Group.find(id)
+            current_reader.groups << group unless !group.ancestors.include?(conference_group)
+          end
+        end
+        
+      end
+      flash[:notice] = "Your conference subscription has been updated"
+      
+    # end
+    redirect_to membership_details_path
+  end
+  
+  def conference_group
+    Group.find((Radiant::Config['conference_group_id']|| 257).to_i)
+  end  
+  helper_method :conference_group
 end
