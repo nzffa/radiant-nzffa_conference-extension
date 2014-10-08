40df684df7a4e1bff6841967309c1982dd0fb322
Benny Degezelle <hi@monkeypatch.be>
2014-10-08 17:35:12 +0200

    Add reader.is_registered_for_conference?

--- lib/conference/reader_extension.rb
+++ lib/conference/reader_extension.rb
@@ -2,4 +2,8 @@ module Conference::ReaderExtension
   def self.included(base)
     base.send(:has_one, :conference_subscription)
   end
+  
+  def is_registered_for_conference?
+    conference_subscription && (conference_subscription.levy == conference_subscription.paid_amount)
+  end
 end
