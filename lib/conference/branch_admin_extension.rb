cbd1806163484688c3e2ff211c2b3625d04f75b1
Benny Degezelle <hi@monkeypatch.be>
2014-11-06 11:11:56 +0100

    Avoid nil-related error in branch_admin for non-conference groups

--- lib/conference/branch_admin_extension.rb
+++ lib/conference/branch_admin_extension.rb
@@ -4,7 +4,7 @@ module Conference::BranchAdminExtension
     klass.class_eval do
       
       def edit_with_conference_hook
-        Reader.find_by_nzffa_membership_id(params[:nzffa_membership_id]).conference_subscription.paid_online? ? redirect_to(branch_admin_path(@template.conference_group)) : edit_without_conference_hook
+        Reader.find_by_nzffa_membership_id(params[:nzffa_membership_id]).conference_subscription.try(:paid_online?) ? redirect_to(branch_admin_path(@template.conference_group)) : edit_without_conference_hook
       end
       alias_method_chain :edit, :conference_hook
       
