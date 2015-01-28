1966aa2fae1678e3da3608e2d57add82e184e34a
Benny Degezelle <hi@monkeypatch.be>
2015-01-28 06:09:13 +0100

    Add partner rows to xls/csv downloads

--- lib/conference/branch_admin_extension.rb
+++ lib/conference/branch_admin_extension.rb
@@ -21,6 +21,11 @@ module Conference::BranchAdminExtension
             @readers.each do |r|
               next unless r.conference_subscription
               csv << [r.nzffa_membership_id, r.name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:levy), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group?}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", r.groups.select{|g| g.parent && g.parent != @template.conference_group}.map{|g| g.name}.join(", ") ]
+              if r.conference_subscription.couple? && (r.conference_subscription.partner_group_ids.try(:include?, @group.id) || [@group.id, @group.parent_id].include?(Radiant::Config['conference_group_id'].to_i))
+                # Add row for partner;
+                options_string = r.conference_subscription.partner_group_ids.nil? ? r.groups.select{|g| g.parent && g.parent != @template.conference_group}.map{|g| g.name}.join(", ") : Group.find(r.conference_subscription.partner_group_ids).map{|g| g.name}.join(", ")
+                csv << [r.nzffa_membership_id, r.conference_subscription.partner_name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:levy), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group?}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", options_string ]
+              end
             end
           end
           
@@ -43,8 +48,10 @@ module Conference::BranchAdminExtension
           sheet.row(0).replace(["#{@group.name} downloaded #{Time.now.strftime("%Y-%m-%d")}"])
           sheet.row(1).replace(columns.map{|k| k.capitalize})
           
-          @readers.select{|r| r.conference_subscription }.each_with_index do |reader, i|
-            sheet.row(i+2).replace(columns.map do |k|
+          next_row_index = 2
+          
+          @readers.select{|r| r.conference_subscription }.each do |reader|
+            sheet.row(next_row_index).replace(columns.map do |k|
               case k
               when 'payment_method', 'notes', 'levy', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
               when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
@@ -59,6 +66,34 @@ module Conference::BranchAdminExtension
                 reader.send(k)
               end
             end)
+            next_row_index += 1
+            
+            if reader.conference_subscription.couple? && (reader.conference_subscription.partner_group_ids.try(:include?, @group.id) || [@group.id, @group.parent_id].include?(Radiant::Config['conference_group_id'].to_i))
+              # Add partner row
+              sheet.row(next_row_index).replace(columns.map do |k|
+                case k
+                when 'payment_method', 'notes', 'levy', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
+                when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
+                when 'registrants' then reader.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1
+                when 'postal_address' then reader.postal_address_string
+                when 'registered_for' then reader.groups.select{|g| g.is_conference_group?}.map{|g| g.name}.join(", ")
+                when 'full_registration' then
+                  reader.groups.include?(@template.conference_group) ? "Full" : "Partial"
+                when 'day_options' then
+                  if reader.conference_subscription.partner_group_ids.nil?
+                    reader.groups.select{|g| g.parent && g.parent != @template.conference_group}.map{|g| g.name}.join(", ")
+                  else
+                    Group.find(reader.conference_subscription.partner_group_ids).map{|g| g.name}.join(", ")
+                  end
+                when 'name'
+                  reader.conference_subscription.partner_name
+                else
+                  reader.send(k)
+                end
+              end)
+              next_row_index += 1
+            end
+            
           end
     
           filename = "#{@group.name}-#{Time.now.strftime("%Y-%m-%d")}.xls"
