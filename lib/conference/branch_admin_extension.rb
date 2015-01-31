6703115f1fc5a178dda07b584e4488de8f81ab17
Benny Degezelle <hi@monkeypatch.be>
2015-01-31 17:12:15 +0100

    Fix typo..

--- lib/conference/branch_admin_extension.rb
+++ lib/conference/branch_admin_extension.rb
@@ -15,12 +15,12 @@ module Conference::BranchAdminExtension
             @readers.each do |r|
               next unless r.conference_subscription
               if !r.conference_subscription.couple? || r.conference_subscription.group_ids.map(&:to_i).include?(@group.id)
-                csv << [r.nzffa_membership_id, r.name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:amount_paid), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group? && r.conference_subscription.group_ids.map(&:to_i).include?(g.id)}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", r.groups.select{|g| g.is_conference_day_option? && r.conference_subscription.group_ids.include?(g.id)}.map{|g| g.name}.join(", ") ]
+                csv << [r.nzffa_membership_id, r.name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:paid_amount), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group? && r.conference_subscription.group_ids.map(&:to_i).include?(g.id)}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", r.groups.select{|g| g.is_conference_day_option? && r.conference_subscription.group_ids.include?(g.id)}.map{|g| g.name}.join(", ") ]
               end
               if r.conference_subscription.couple? && (r.conference_subscription.partner_group_ids.try(:include?, @group.id) || [@group.id, @group.parent_id].include?(Radiant::Config['conference_group_id'].to_i))
                 # Add row for partner;
                 options_string = r.conference_subscription.partner_group_ids.nil? ? r.groups.select{|g| g.is_conference_day_option?}.map{|g| g.name}.join(", ") : Group.find(r.conference_subscription.partner_group_ids).map{|g| g.name}.join(", ")
-                csv << [r.nzffa_membership_id, r.conference_subscription.partner_name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:amount_paid), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group? && (!g.is_conference_day_option? || r.conference_subscription.partner_group_ids.to_a.map(&:to_i).include?(g.id)) }.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", options_string ]
+                csv << [r.nzffa_membership_id, r.conference_subscription.partner_name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:paid_amount), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group? && (!g.is_conference_day_option? || r.conference_subscription.partner_group_ids.to_a.map(&:to_i).include?(g.id)) }.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", options_string ]
               end
             end
           end
@@ -51,7 +51,7 @@ module Conference::BranchAdminExtension
               sheet.row(next_row_index).replace(columns.map do |k|
                 case k
                 when 'payment_method', 'notes', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
-                when 'levy' then reader.conference_subscription.try(:amount_paid)
+                when 'levy' then reader.conference_subscription.try(:paid_amount)
                 when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
                 when 'registrants' then reader.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1
                 when 'postal_address' then reader.postal_address_string
@@ -71,7 +71,7 @@ module Conference::BranchAdminExtension
               sheet.row(next_row_index).replace(columns.map do |k|
                 case k
                 when 'payment_method', 'notes', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
-                when 'levy' then reader.conference_subscription.try(:amount_paid)
+                when 'levy' then reader.conference_subscription.try(:paid_amount)
                 when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
                 when 'registrants' then reader.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1
                 when 'postal_address' then reader.postal_address_string
