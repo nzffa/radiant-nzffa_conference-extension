3ffc8b1368d40b727ccd8b9d42609dc1d6c7e852
Benny Degezelle <hi@monkeypatch.be>
2015-01-31 16:41:46 +0100

    Stop preventing a conference subscription from being edited because it was paid online..

--- lib/conference/branch_admin_extension.rb
+++ lib/conference/branch_admin_extension.rb
@@ -2,12 +2,6 @@ module Conference::BranchAdminExtension
   
   def self.included(klass)
     klass.class_eval do
-      
-      def edit_with_conference_hook
-        Reader.find_by_nzffa_membership_id(params[:nzffa_membership_id]).conference_subscription.try(:paid_online?) ? redirect_to(branch_admin_path(@template.conference_group)) : edit_without_conference_hook
-      end
-      alias_method_chain :edit, :conference_hook
-      
       def render_csv_of_readers_with_conference_hook
         if RUBY_VERSION =~ /1.9/
           require 'csv'
