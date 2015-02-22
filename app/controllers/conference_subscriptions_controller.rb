e53e2034f60f10031ff79130a279fd1e0d6aaea1
Benny Degezelle <hi@monkeypatch.be>
2015-02-22 14:58:50 +0100

    All conference subscriptions xls/csv: include partners

--- app/controllers/conference_subscriptions_controller.rb
+++ app/controllers/conference_subscriptions_controller.rb
@@ -8,20 +8,21 @@ class ConferenceSubscriptionsController < ReaderActionController
     
     respond_to do |format|
       format.xls do
-        columns = %w(nzffa_membership_id name email phone postal_address post_city payment_method date_paid registrants levy notes do_not_publish_contact_details first_conference pickup_from_incoming_flight pickups_from_to_conference registered_for full_registration day_options)
+        columns = %w(nzffa_membership_id name email phone postal_address post_city payment_method date_paid levy notes do_not_publish_contact_details first_conference pickup_from_incoming_flight pickups_from_to_conference registered_for full_registration day_options)
         require 'spreadsheet'
         book = Spreadsheet::Workbook.new
         sheet = book.create_worksheet :name => 'Readers export'
         
         sheet.row(0).replace(["All conference subscriptions - downloaded #{Time.now.strftime("%Y-%m-%d")}"])
         sheet.row(1).replace(columns.map{|k| k.capitalize})
+        next_row_index = 2
         
         @readers.select{|r| r.conference_subscription }.each_with_index do |reader, i|
-          sheet.row(i+2).replace(columns.map do |k|
+          sheet.row(next_row_index).replace(columns.map do |k|
             case k
-            when 'payment_method', 'notes', 'levy', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
+            when 'payment_method', 'notes', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
+            when 'levy' then reader.conference_subscription.try(:paid_amount)
             when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
-            when 'registrants' then reader.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1
             when 'postal_address' then reader.postal_address_string
             when 'registered_for' then reader.groups.select{|g| g.is_conference_group?}.map{|g| g.name}.join(", ")
             when 'full_registration' then
@@ -32,6 +33,33 @@ class ConferenceSubscriptionsController < ReaderActionController
               reader.send(k)
             end
           end)
+          next_row_index += 1
+          if reader.conference_subscription.couple?
+            # Add partner row
+            sheet.row(next_row_index).replace(columns.map do |k|
+              case k
+              when 'payment_method', 'notes', 'do_not_publish_contact_details', 'first_conference', 'pickup_from_incoming_flight', 'pickups_from_to_conference' then reader.conference_subscription.try(:send, k).to_s
+              when 'levy' then ""
+              when 'date_paid' then reader.conference_subscription.try(:paid_at).try(:strftime, "%b %d")
+              when 'postal_address' then reader.postal_address_string
+              when 'registered_for' then reader.groups.select{|g| g.is_conference_group? && (!g.is_conference_day_option? || reader.conference_subscription.partner_group_ids.to_a.map(&:to_i).include?(g.id))}.map{|g| g.name}.join(", ")
+              when 'full_registration' then
+                reader.groups.include?(@template.conference_group) ? "Full" : "Partial"
+              when 'day_options' then
+                if reader.conference_subscription.partner_group_ids.nil?
+                  reader.groups.select{|g| g.is_conference_day_option?}.map{|g| g.name}.join(", ")
+                else
+                  Group.find(reader.conference_subscription.partner_group_ids).map{|g| g.name}.join(", ")
+                end
+              when 'name'
+                reader.conference_subscription.partner_name
+              else
+                reader.send(k)
+              end
+            end)
+            next_row_index += 1
+          end
+          
         end
   
         filename = "All conference subscriptions-#{Time.now.strftime("%Y-%m-%d")}.xls"
@@ -47,10 +75,15 @@ class ConferenceSubscriptionsController < ReaderActionController
           csv_lib = FasterCSV
         end
         csv_string = csv_lib.generate do |csv|
-          csv << %w[nzffa_membership_id name email phone postal_address post_city paid_by date_paid registrants levy notes do_not_publish_contact_details first_conference pickup_from_incoming_flight pickups_from_to_conference registered_for full_registration day_options]
+          csv << %w[nzffa_membership_id name email phone postal_address post_city paid_by date_paid levy notes do_not_publish_contact_details first_conference pickup_from_incoming_flight pickups_from_to_conference registered_for full_registration day_options]
           @readers.each do |r|
             next unless r.conference_subscription
-            csv << [r.nzffa_membership_id, r.name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:single_or_couple) == 'couple' ? 2 : 1, r.conference_subscription.try(:levy), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group?}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", r.groups.select{|g| g.parent && g.parent != @template.conference_group}.map{|g| g.name}.join(", ") ]
+            csv << [r.nzffa_membership_id, r.name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:paid_amount), r.conference_subscription.try(:notes), r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group?}.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", r.groups.select{|g| g.parent && g.parent != @template.conference_group}.map{|g| g.name}.join(", ") ]
+            if r.conference_subscription.couple?
+              # Add row for partner;
+              options_string = r.conference_subscription.partner_group_ids.nil? ? r.groups.select{|g| g.is_conference_day_option?}.map{|g| g.name}.join(", ") : Group.find(r.conference_subscription.partner_group_ids).map{|g| g.name}.join(", ")
+              csv << [r.nzffa_membership_id, r.conference_subscription.partner_name, r.email, r.phone, r.postal_address_string, r.post_city, r.conference_subscription.try(:payment_method), r.conference_subscription.try(:paid_at).try(:strftime, "%b %d"), r.conference_subscription.try(:paid_amount), "", r.conference_subscription.try(:do_not_publish_contact_details), r.conference_subscription.try(:first_conference), r.conference_subscription.try(:pickup_from_incoming_flight), r.conference_subscription.try(:pickups_from_to_conference), r.groups.select{|g| g.is_conference_group? && (!g.is_conference_day_option? || r.conference_subscription.partner_group_ids.to_a.map(&:to_i).include?(g.id)) }.map{|g| g.name }.join(", "), r.groups.include?(@template.conference_group) ? "Full" : "Partial", options_string ]
+            end
           end
         end
 
