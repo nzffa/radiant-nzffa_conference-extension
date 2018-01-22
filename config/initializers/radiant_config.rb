92f38798d12936c39eb60fea42498336b7295e16
Benny Degezelle <hi@monkeypatch.be>
2018-01-22 21:42:49 +0100

    Extend configurability...
    Allow to enable/disable 'pickup from airport' asking
    Allow what is showed on the receipt as labels to airport arrival and hotel info

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,5 +1,8 @@
 Radiant.config do |config|
   config.namespace('conference') do |conference|
     conference.define 'root_group_id', :select_from => lambda {Group.all.map{|l| [l.name, l.id.to_s]}}, :allow_blank => true
+    conference.define 'ask_flight_info_for_pickup', :default => false, :type => :boolean
+    conference.define 'receipt_label_flight_pickup', :default => 'Your arrival time and flight, for pickup from the airport:'
+    conference.define 'receipt_label_hotel', :default => 'You will be staying at:'
   end
 end
